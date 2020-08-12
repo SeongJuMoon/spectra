af0824acc199e543402015fdc23e1df50758f43e
SeongJu Moon <arcuer.dev@gmail.com>
2020-08-13 00:40:14 +0900

    change to make sense

--- prometheus-install.sh
+++ prometheus-install.sh
@@ -1,8 +1,8 @@
 #bin/bash
 helm install prometheus \
 --set server.persistentVolume.existingClaim="prometheus-server" \
---set alertmanager.persistentVolume.existingClaim="prometheus-alert-manager" \
---set pushgateway.persistentVolume.existingClaim="pushgw" \
+--set alertmanager.persistentVolume.existingClaim="prometheus-alertmanager" \
+--set pushgateway.persistentVolume.existingClaim="pushgateway" \
 --set server.securityContext.fsGroup=2000 \
 --set server.securityContext.runAsUser=1000 \
 --version 11.6.0 \
