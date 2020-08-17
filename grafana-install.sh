0adf544487f911adc912e905c8d15501e2e951f7
SeongJu Moon <arcuer.dev@gmail.com>
2020-08-17 22:23:42 +0900

    unused name delete

--- grafana-install.sh
+++ grafana-install.sh
@@ -3,9 +3,6 @@ helm install grafana \
 --set persistence.enabled=true \
 --set persistence.existingClaim=grafana \
 --set service.type=LoadBalancer \
---set service.port=80 \
---set service.portName=lb-grafana \
---set service.targetPort=3000 \
 --set tolerations[0].key=node-role.kubernetes.io/master \
 --set tolerations[0].effect=NoSchedule \
 --set tolerations[0].operator=Exists \
