7065650125cd097d42710b88f841c5ca022054f9
SeongJu Moon <arcuer.dev@gmail.com>
2020-08-17 21:13:00 +0900

    setup nodeSelector to dedicate pods schedule

--- grafana-install.sh
+++ grafana-install.sh
@@ -1,4 +1,4 @@
-#bin/bash
+#!/bin/bash
 helm install grafana \
 --set persistence.enabled=true \
 --set persistence.existingClaim=grafana \
@@ -6,5 +6,9 @@ helm install grafana \
 --set service.port=80 \
 --set service.portName=lb-grafana \
 --set service.targetPort=3000 \
+--set tolerations[0].key=node-role.kubernetes.io/master \
+--set tolerations[0].effect=NoSchedule \
+--set tolerations[0].operator=Exists \
+--set nodeSelector."kubernetes\.io/hostname"=m-k8s \
 --version 5.3.0 \
 stable/grafana
